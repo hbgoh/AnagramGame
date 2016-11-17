9934a060b371f380613bc385dc2108c1e9fdbe2d
hongbee <hongbee@hongbee>
2016-11-17 11:18:24 +0800

    First release anagramGame v0101

---
+++ src/com/toy/anagrams/util/Java1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.util;
+
+import com.toy.anagrams.ui.Anagrams;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author hongbee
+ */
+public class Java1 {
+    
+    
+    public static void main(String[] args) {
+         System.out.println("Hi is me");
+    }
+    
+}
